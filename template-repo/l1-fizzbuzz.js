9e3b26afeef48863170f0caf11ff2fb4b641ee3d
Pelle Borst <43679981+PelleBorst@users.noreply.github.com>
2019-02-14 14:36:14 +0100

    Update l1-fizzbuzz.js

--- template-repo/l1-fizzbuzz.js
+++ template-repo/l1-fizzbuzz.js
@@ -1,1 +1,18 @@
-r
+for (var list = 1; list <= 100; list++){
+    var leeg = " "
+    if(list % 3 == 0 && list % 5 == 0){
+        leeg += "FizzBuzz";
+
+    } else if (list % 3 == 0){
+        leeg += "Fizz";
+
+    } else if (list % 5 == 0){
+        leeg += "Buzz";
+
+    } else {
+        leeg = list;
+    }
+
+    console.log(leeg);
+
+}
