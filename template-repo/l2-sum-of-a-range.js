aefef163519019a5548b2d31f3df072cc62bd215
Pelle Borst <43679981+PelleBorst@users.noreply.github.com>
2019-02-15 18:03:01 +0100

    Update l2-sum-of-a-range.js

--- template-repo/l2-sum-of-a-range.js
+++ template-repo/l2-sum-of-a-range.js
@@ -1,1 +1,25 @@
- 
+ function range(start, end){
+    var range = [];
+
+    for (let i = start; i <= end, i++)
+        range.push(i);
+
+    returm range;
+}
+
+
+
+
+function sum(array) {
+    var total = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        total += array[i];
+  }
+  return total;
+}
+
+
+console.log(range(1, 10));
+console.log(range(5, 2, -1));
+console.log(sum(range(1, 10)));
