414e8ffca3dd30182dcbbc5870b4d79575b4fabb
Pelle Borst <43679981+PelleBorst@users.noreply.github.com>
2019-02-15 18:01:47 +0100

    Update l2-minimum.js

--- template-repo/l2-minimum.js
+++ template-repo/l2-minimum.js
@@ -1,1 +1,10 @@
- 
+ let lowestNumber;
+
+function minimum(a, b, c, d){
+    // use math.min for returning the lowest number
+    lowestNumber = Math.min(a, b, c, d);
+    return lowestNumber;
+}
+
+console.log(minimum(4345, 127, 65, -10));
+// -10
