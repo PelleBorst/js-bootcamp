a585a63eb960584e7a2c5a670db4a0484ca7edb7
Pelle Borst <43679981+PelleBorst@users.noreply.github.com>
2019-02-15 18:04:01 +0100

    Update l3-vector-type.js

--- template-repo/l3-vector-type.js
+++ template-repo/l3-vector-type.js
@@ -1,1 +1,30 @@
- 
+ class Vec{
+
+    constructor(y, x){
+        this.y = y;
+        this.x = x;
+    }
+
+    plus(vector){
+        this.y += vector.y;
+        this.x += vector.x;
+        return this;
+    }
+
+    minus(vector){
+        this.y -= vector.y;
+        this.x -= vector.x
+        return this;
+    }
+
+        get length() {
+        return(Math.sqrt((this.x * this. x) + (this.y * this.y)));
+    }
+}
+
+console.log(new Vec(1, 2).plus(new Vec(2, 3)));
+// → Vec{x: 3, y: 5}
+console.log(new Vec(1, 2).minus(new Vec(2, 3)));
+// → Vec{x: -1, y: -1}
+console.log(new Vec(3, 4).length);
+// → 5
